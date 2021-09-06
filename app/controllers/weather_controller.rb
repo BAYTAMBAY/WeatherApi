91c1f0e3bda14e19bd6f26624d9e994829684705
salim <tambaysalimst@gmail.com>
2021-09-06 03:18:08 +0300

    degişken isimleri düzenlendi

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,11 +1,11 @@
 class WeatherController < ApplicationController
   def api
     require 'json'
-    @sa=params[:search]
-    if @sa == nil
+    @sehir=params[:search]
+    if @sehir == nil
       uri = URI("https://api.openweathermap.org/data/2.5/weather?q=Kahramanmaras&appid=59c84841fc945dc51dcf4178705e2d5b")
     else
-      uri = URI("https://api.openweathermap.org/data/2.5/weather?q=+#{@sa}+&appid=59c84841fc945dc51dcf4178705e2d5b")
+      uri = URI("https://api.openweathermap.org/data/2.5/weather?q=+#{@sehir}+&appid=59c84841fc945dc51dcf4178705e2d5b")
     end
     
     @a = Net::HTTP.get(uri)
